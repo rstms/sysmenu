fdcc56070ebb99843f6ef6149676786070e04373
Matt Krueger <mkrueger@rstms.net>
2025-10-01 07:37:25 -0600

    Bump version: 0.0.2 → 0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.2",
+	Version: "0.0.3",
 	Use:     "sysmenu",
 	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
